7d7d14093b9fcce5ecad19bdf22f2ad385c5c197
Eric London <github@ericlondon.com>
2019-01-12 07:38:53 -0500

    update posts controller params

--- api/app/controllers/api/posts_controller.rb
+++ api/app/controllers/api/posts_controller.rb
@@ -1,30 +1,26 @@
+# frozen_string_literal: true
+
 class Api::PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update, :destroy]
+  before_action :set_post, only: %i[show update destroy]
 
-  # GET /posts
   def index
-    @posts = Post.all
-
-    render json: @posts
+    render json: Post.all
   end
 
-  # GET /posts/1
   def show
     render json: @post
   end
 
-  # POST /posts
   def create
-    @post = Post.new(post_params)
+    post = Post.new(post_params)
 
-    if @post.save
-      render json: @post, status: :created, location: @post
+    if post.save
+      render json: post, status: :created
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: post.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
       render json: @post
@@ -33,19 +29,17 @@ class Api::PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
   def destroy
     @post.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-      params.fetch(:post, {})
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
