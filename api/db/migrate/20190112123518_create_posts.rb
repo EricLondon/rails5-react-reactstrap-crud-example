e00a070175c6d959fd0fa7dd42a84bc56405f13b
Eric London <github@ericlondon.com>
2019-01-13 15:35:39 -0500

    forgot to make fields non null on post model

--- api/db/migrate/20190112123518_create_posts.rb
+++ api/db/migrate/20190112123518_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :body
+      t.string :title, null: false
+      t.string :body, null: false
 
       t.timestamps
     end
